7ee47865dbd6182a9856ef955faf731ad65a7604
Reddsito <ptyelnegro@gmail.com>
2025-01-20 14:50:49 -0500

    feat(web): add ui components and nomination functionality
    Added a collection of React components for the nomination process:
    - Form and lists for nominations and participants
    - Result visualization components
    - Basic UI components (BottomSheet, Dialog, Counter, etc.)
    - Removed unused svg files

---
+++ apps/web/src/components/ResultLists.tsx
@@ -0,0 +1,26 @@
+// import React from "react";
+// import ResultCard from "./ui/ResultCard";
+// import HorizontalSwipeList from "./ui/HorizontalSwipeList";
+// import { RoundResult } from "shared/poll-types";
+
+// type ResultsList = {
+// 	results: Readonly<RoundResult[]>;
+// };
+
+// const ResultsList: React.FC<ResultsList> = ({ results }) => {
+// 	return (
+// 		<div className="mx-auto max-h-full flex flex-col">
+// 			<HorizontalSwipeList>
+// 				{results.map((result, i) => (
+// 					// Can use index as we'll never change list
+// 					<ResultCard
+// 						key={i}
+// 						result={result}
+// 					/>
+// 				))}
+// 			</HorizontalSwipeList>
+// 		</div>
+// 	);
+// };
+
+// export default ResultsList;
